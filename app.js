7e9de2d2b6fe053476aebff071aaf271ec7d897c
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-04-17 11:59:46 +0700

    faet: implement the function of setting a specific slide as the active one

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const slides = document.querySelectorAll('.slide');
-
-// используя цикл for..of
-for (const slide of slides) {
-  slide.addEventListener('click', () => {
-    clearActiveClasses();
-    slide.classList.add('active');
-  })
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.slide');
+  
+  slides[activeSlide].classList.add('active');
+  
+  // используя цикл for..of
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses();
+      slide.classList.add('active');
+    })
+  }
+  
+  // используя метод forEach()
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    })
+  }
 }
 
-// используя метод forEach()
-function clearActiveClasses() {
-  slides.forEach((slide) => {
-    slide.classList.remove('active');
-  })
-}
+slidesPlugin(1);
